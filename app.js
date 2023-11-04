99a176853d628f849beb67d2d5912954ca33a9f6
Madeline Taylor <maddyjunetaylor@gmail.com>
2023-11-03 23:33:30 -0400

    finished CSS and html

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const totalElement = document.getElementById('total');
-
-
-var clicks = 0
-function onClick(){
+let autoClicker = document.getElementById('autoClickBtn');
+let clicks = 0
+function donutClick(){
     clicks += 1;
-    document.getElementById("total").innerHTML = clicks
+    totalElement.innerHTML = clicks
 }
+let autoClickCount = 0;
+let autoClickPrice = 10;
+function purchaseAutoClick(){
+    if (clicks >= autoClickPrice) {
+        clicks -= autoClickPrice;
+        autoClickPrice = Math.round(autoClickPrice * 1.2);
+        autoClickCount ++;
+        totalElement.innerHTML = clicks
+        setInterval();
+    }
+}
+setInterval(() => {
+    clicks += autoClickCount;
+    totalElement.innerHTML = clicks
+}, 1000);
